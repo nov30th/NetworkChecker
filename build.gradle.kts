2981c35d093daa287fbad954f7ef9199620fc94f
Vincent Qiu (HOHO``) <nov30th@gmail.com>
2020-08-03 04:55:34 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 }
 //
